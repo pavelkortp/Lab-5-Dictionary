c003763fb4954356f2373b7f3c2a07bbb824e064
Pavlo <pavelkortp@gmail.com>
2023-11-08 15:04:25 +0200

    Adds comment how to use files

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,6 +2,8 @@
 
 var resultTable = new GameResultTable();
 
+//Files must be placed at bin/Debug/net7.0/
+
 resultTable.AddGamesFromFile("games.txt");
 
 Console.WriteLine(resultTable.GetResults());
